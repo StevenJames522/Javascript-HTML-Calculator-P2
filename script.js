bbb5cb9540dd185dddc61521eb7e850a1810af45
Steven James <sjames@wlapcs.org>
2017-02-28 04:10:58 +0000

    my second commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function clearDisplay(){
-    document.getElementById("display-bar").innerHTML =""; 
+    document.getElementById("display-bar").value =""; 
 }
 
 function display(userInput){
-    document.getElementById("display-bar").innerHTML += userInput; 
+    document.getElementById("display-bar").value += userInput; 
 }
 
 function calculate(){
-    var answer = eval(document.getElementById("display-bar").innerHTML);
-    document.getElementById("display-bar").innerHTML = answer;
+    var answer = eval(document.getElementById("display-bar").value);
+    document.getElementById("display-bar").value = answer;
 }
